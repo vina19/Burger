375c74d4d6516dedfaffce9374782a5a714a584b
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2021-01-07 13:44:33 -0600

    Created a connection with mysql for our ORM to use

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,22 @@
+// Set up MySQL connection.
 const mysql = require('mysql');
 
+let connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "cat_db"
+  });
+  
+  // Make connection.
+  connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+
+// Export conneciton for our ORM to use.
 module.exports = connection;
