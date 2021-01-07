83a9c666ec31109d07c3c4dd57d8a3aa18f94191
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2021-01-07 11:28:23 -0600

    Created burgers_db database and burgers table

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+/* Delete the database if there is database called burgers_db before creating it. */
+DROP DATABASE IF EXISTS burgers_db;
+
+/* Create the database burgers_db. */
+CREATE DATABASE burgers_db;
+
+/* Specify which database that we are going to use. */
+USE burgers_db;
+
+/* Burgers Table */
+CREATE TABLE burgers (
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(30),
+    devoured BOOLEAN,
+    PRIMARY KEY (id) 
+);
