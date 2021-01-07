4f52b769e88ffadf13b44d15e6cc96bc32381245
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2021-01-07 12:06:04 -0600

    Created file that has the code that will call the ORM functions using burger input for the ORM

---
+++ models/burger.js
@@ -0,0 +1,15 @@
+const orm = require('../config/orm');
+
+let burger = {
+    selectAll: function() {
+
+    },
+    insertOne: function() {
+
+    },
+    updateOne: function() {
+
+    }
+};
+
+module.exports = burger;
