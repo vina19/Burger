8094a7755d88c647dbc017c9f3518e9f9cfb9d33
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2021-01-07 18:30:34 -0600

    Created the helper function for SQL syntax

--- config/orm.js
+++ config/orm.js
@@ -1,4 +1,17 @@
+// Import MySQL connection.
 const connection = require('./connection');
+
+// Helper function to SQL syntax.
+// Loops through and creates an array of questions marks and turns it into a string.
+function printQuestionMarks(num) {
+    let array = [];
+
+    for (let i=0; i < num; i++) {
+        array.push("?");
+    };
+
+    return array.toString();
+};
 
 let orm = {
     selectAll: function() {
