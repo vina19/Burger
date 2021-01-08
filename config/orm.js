af2c5ea6b6cd5e80fda3ee49efef26450b7ad009
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2021-01-07 20:18:46 -0600

    Created the helper for converting key/values as a string int array

--- config/orm.js
+++ config/orm.js
@@ -13,6 +13,29 @@ function printQuestionMarks(num) {
     return array.toString();
 };
 
+// Helper function to convert object key/value pairs to SQL syntax.
+function objToSql(ob) {
+    let array = [];
+
+    // Looping through the keys and push the key/value as a string int array.
+    for (let key in ob) {
+        let value = ob[key];
+
+        // Check to skip hidden properties.
+        if (Object.hasOwnProperty.call(ob, key)) {
+            
+            // if string with spaces, add quotations.
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            };
+
+            array.push(key + "=" + value);
+        };
+    };
+    return array.toString();
+};
+
+// Object for all our SQL statement functions.
 let orm = {
     selectAll: function() {
 
