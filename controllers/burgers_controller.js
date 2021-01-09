caddf60407eeb2167c54e7f5d63ce892384b90f6
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2021-01-08 21:41:18 -0600

    Created router get all the burgers data

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,5 +1,19 @@
 const express = require('express');
-const burger = require('../models/burger');
+
 const router = express.Router();
+
+// Export the database functions.
+const burger = require('../models/burger');
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+      let hbsObject = {
+        burgers: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+});
 
 module.exports = router;
