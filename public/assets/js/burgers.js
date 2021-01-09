5a6703ccb937e8afa77869d6903fce9abe205276
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2021-01-09 13:48:14 -0600

    Created the event handlers for change-devoured button

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,24 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $(".change-devoured").on("click", function(event) {
+      var id = $(this).data("id");
+      var newDevoured = $(this).data("newDevoured");
+  
+      var newDevouredState = {
+        devoured: newDevoured
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newDevouredState
+      }).then(
+        function() {
+          console.log("changed devoured to", newDevoured);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+});
+  
