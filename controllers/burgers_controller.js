ecdc7fa358a403b03a8be3b0488f1452c9242e83
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2021-01-08 21:43:45 -0600

    Created a post to add burger data to the database

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,6 +6,7 @@ const router = express.Router();
 const burger = require('../models/burger');
 
 // Create all our routes and set up logic within those routes where required.
+// GET burgers database.
 router.get("/", function(req, res) {
     burger.all(function(data) {
       let hbsObject = {
@@ -13,6 +14,19 @@ router.get("/", function(req, res) {
       };
       console.log(hbsObject);
       res.render("index", hbsObject);
+    });
+});
+
+// POST burger data to the database.
+router.post("/api/burgers", function(req, res) {
+    burger.create([
+      "burger_name", "devoured"
+    ], [
+      req.body.burger_name, req.body.devoured
+    ], function(result) {
+
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
     });
 });
 
