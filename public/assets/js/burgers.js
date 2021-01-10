f6ac045f7468610cd07a12d50e8e31f2da0bffd4
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2021-01-09 21:25:25 -0600

    Created index handlebars

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -20,5 +20,39 @@ $(function() {
         }
       );
     });
+
+    $(".create-form").on("submit", function(event) {
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();
+    
+        var newCat = {
+          name: $("#ca").val().trim(),
+          sleepy: $("[name=sleepy]:checked").val().trim()
+        };
+    
+        // Send the POST request.
+        $.ajax("/api/cats", {
+          type: "POST",
+          data: newCat
+        }).then(
+          function() {
+            console.log("created new cat");
+            // Reload the page to get the updated list
+            location.reload();
+          }
+        );
+      });
+    
+      $(".delete-btn").on("click", function(event) {
+    
+        let id = $(this).data("id");
+    
+        $.ajax("/api/cats/" + id, {
+          type: "DELETE",
+        }).then(function() {
+          console.log("This cat was successfully deleted", id);
+          location.reload();
+        });
+      });
 });
   
