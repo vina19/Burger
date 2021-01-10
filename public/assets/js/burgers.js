4428e82556d4e19f8774882bce9b4a5059b3d680
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2021-01-09 22:07:29 -0600

    Edited the indentation

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,24 +1,22 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
     $(".change-devoured").on("click", function(event) {
-      var id = $(this).data("id");
-      var newDevoured = $(this).data("newDevoured");
+        let id = $(this).data("id");
+        let newDevoured = $(this).data("newdevoured");
   
-      var newDevouredState = {
-        devoured: newDevoured
-      };
+        let newDevouredState = {
+            devoured: newDevoured
+        };
   
-      // Send the PUT request.
-      $.ajax("/api/burgers/" + id, {
-        type: "PUT",
-        data: newDevouredState
-      }).then(
-        function() {
-          console.log("changed devoured to", newDevoured);
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
+        // Send the PUT request.
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newDevouredState
+        }).then(function() {
+            console.log("changed devoured to", newDevoured);
+            // Reload the page to get the updated list
+            location.reload();
+        });
     });
 
     // Event handler for devour me button
@@ -28,7 +26,6 @@ $(function() {
     
         let newBurger = {
           burger_name: $("#burger-name").val().trim(),
-          devoured: 0
         };
     
         // Send the POST request.
