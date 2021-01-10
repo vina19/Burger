886f058388572e9931db6283a00e5e6cc5f4f999
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2021-01-09 21:42:46 -0600

    Fixed the error controllers file cannot find

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-const express = require('express');
-
 let express = require("express");
 
 let PORT = process.env.PORT || 8080;
@@ -20,7 +18,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-let routes = require("./controllers/burgersController.js");
+let routes = require("./controllers/burgers_controller");
 
 app.use(routes);
 
